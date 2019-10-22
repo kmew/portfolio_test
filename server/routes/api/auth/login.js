fac498db9a715d35d8c74d0d384ab6cdd73bc204
KMOW <tissatatc@gmail.com>
2019-10-22 20:08:01 +0700

    EDIT: Update

--- server/routes/api/auth/login.js
+++ server/routes/api/auth/login.js
@@ -1,52 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
-const app = express()
 const bcrypt = require('bcryptjs')
 
 let User = require('../../../auth.model')
-
-
-/* PASSPORT SETUP */
-app.use(passport.initialize())
-app.use(passport.session())
-
-app.get('/success', (req, res) => res.send("Welcome "+req.query.username+"!!"))
-app.get('/error', (req, res) => res.send("error logging in"))
-
-passport.serializeUser((user, cb) => {
-    cb(null, user.id);
-})
-
-passport.deserializeUser(function(id, cb) {
-    User.getUserById(id, (err, user) => {
-        done(err, user)
-    })
-})
-
-
-/* PASSPORT LOCAL AUTHENTICATION */
-passport.use(new LocalStrategy(
-    (username, password, done) => {
-        console.log('username test: ',username)
-        console.log('password test: ',password)
-        User.getUserByUsername(username, (err, user) => {
-            if(err) throw err
-            if(!user){
-                return done(null, false, {message: 'Unknow User'})
-            }
-        })
-        User.comparePassword(password, user.password, (err, isMatch) => {
-            if(err) throw err
-            if(isMatch){
-                return done(null, user)
-            } else {
-                return done(null, flase, {message: 'Invalid password'})
-            }
-        })
-    }
-))
 
 router.route('/').post( async (req, res, next) => {
     const { username, password } = req.body
