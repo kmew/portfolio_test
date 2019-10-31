8da0396bf0ee26471b5f605ca8142ec18027eea9
KMOW <tissatatc@gmail.com>
2019-10-31 13:32:04 +0700

    try

--- server/server.js
+++ server/server.js
@@ -11,9 +11,7 @@ const Authen = require('./routes/auth')
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:4000',
-    'http://13.58.199.50',
-    'http://13.58.199.50:3000',
-    'http://13.58.199.50:4000',
+    'http://13.58.199.50'
 ]
 app.use(cors({
     origin: function(origin, callback){
